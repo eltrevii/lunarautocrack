4a98ea942da38e56124f1e0325913c3141426df8
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-11-12 12:42:04 +0100

    additions

--- lunar119auto.bat
+++ lunar119auto.bat
@@ -1,10 +1,13 @@
 @echo off & setlocal EnableDelayedExpansion && set "_v=1.19" & set "_vv=!_v:.=!"
 title Lunar Launcher (%_v%) ^| by aritz331_ for Aritz's Utils - an aritz331_ original series
 if not exist %temp%\.331 (md %temp%\.331) else (attrib -s -h -r %temp%\.331)
-cd %temp%\.331
+pushd %temp%\.331
 
 set "_331=%userprofile%\.331"
-set "_lunarpath=%userprofile%\.331\lunar"
+set "_lunarpath=%userprofile%\.331\lunarauto"
+set "_lunarpath2=%_lunarpath:\=/%"
+echo %_lunarpath2%
+pause
 
 attrib -s -h -r %_331% >nul 2>&1
 attrib -s -h -r %_lunarpath% >nul 2>&1
@@ -20,7 +23,7 @@ if not exist %_lunarpath%\java\  (
 	call :dl-j
 )
 
-rem if not exist %_lunarpath%\lunar\ (
+rem if not exist %_lunarpath%\lunarauto\ (
 	7z >nul 2>&1 || call :dl-7z
 	call :dl-lunar
 rem )
@@ -66,7 +69,7 @@ call :7z-j
 exit /b
 
 :7z-lunar
-7z x -y lunar.7z -o%_lunarpath%\lunar\
+7z x -y lunar.7z -o%_lunarpath%\lunarauto\
 7z x -y lunarag.7z -o%_lunarpath%\agents\
 echo.
 exit /b
@@ -84,12 +87,13 @@ exit /b
 
 :start
 cls
-pushd %_lunarpath%\lunar\offline\multiver
+popd
+pushd %_lunarpath%\lunarauto\offline\multiver
 echo Set username
 set /p "_username=> "
 
 powershell -NoP -W minimized ; exit
-%_lunarpath%\java\bin\java.exe --add-modules jdk.naming.dns --add-exports jdk.naming.dns/com.sun.jndi.dns=java.naming -Djna.boot.library.path=natives -Dlog4j2.formatMsgNoLookups=true --add-opens java.base/java.io=ALL-UNNAMED -Xms4096m -Xmx4096m -Djava.library.path=natives -cp %_lunarpath%/lunar/offline/multiver/argon-0.1.0-SNAPSHOT-all.jar;%_lunarpath%/lunar/offline/multiver/common-0.1.0-SNAPSHOT-all.jar;%_lunarpath%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-all.jar;%_lunarpath%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-v1_19.jar;%_lunarpath%/lunar/offline/multiver/genesis-0.1.0-SNAPSHOT-all.jar;%_lunarpath%/lunar/offline/multiver/Indium_v1_19.jar;%_lunarpath%/lunar/offline/multiver/Iris_v1_19.jar;%_lunarpath%/lunar/offline/multiver/lunar-emote.jar;%_lunarpath%/lunar/offline/multiver/lunar-lang.jar;%_lunarpath%/lunar/offline/multiver/lunar.jar;%_lunarpath%/lunar/offline/multiver/optifine-0.1.0-SNAPSHOT-all.jar;%_lunarpath%/lunar/offline/multiver/Phosphor_v1_19.jar;%_lunarpath%/lunar/offline/multiver/sodium-0.1.0-SNAPSHOT-all.jar;%_lunarpath%/lunar/offline/multiver/Sodium_v1_19.jar;%_lunarpath%/lunar/offline/multiver/v1_19-0.1.0-SNAPSHOT-all.jar "-javaagent:%_lunarpath%/agents/CrackedAccount.jar=%_username%" "-javaagent:%_lunarpath%/agents/CustomAutoGG.jar=" "-javaagent:%_lunarpath%/agents/CustomLevelHead.jar=" "-javaagent:%_lunarpath%/agents/HitDelayFix.jar=" "-javaagent:%_lunarpath%/agents/LevelHeadNicks.jar=" "-javaagent:%_lunarpath%/agents/LunarEnable.jar=" "-javaagent:%_lunarpath%/agents/LunarPacksFix.jar=" "-javaagent:%_lunarpath%/agents/NoPinnedServers.jar=" "-javaagent:%_lunarpath%/agents/RemovePlus.jar=" "-javaagent:%_lunarpath%/agents/StaffEnable.jar=" "-javaagent:%_lunarpath%/agents/TeamsAutoGG.jar=" -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=16M -Djava.net.preferIPv4Stack=true com.moonsworth.lunar.genesis.Genesis --version 1.19 --accessToken 0 --assetIndex 1.19 --userProperties {} --gameDir C:/Users/USER/AppData/Roaming/.minecraft --launcherVersion 2.12.7 --width 720 --height 480 --workingDirectory . --classpathDir . --ichorClassPath %_lunarpath%/lunar/offline/multiver/argon-0.1.0-SNAPSHOT-all.jar,%_lunarpath%/lunar/offline/multiver/common-0.1.0-SNAPSHOT-all.jar,%_lunarpath%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-all.jar,%_lunarpath%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-v1_19.jar,%_lunarpath%/lunar/offline/multiver/genesis-0.1.0-SNAPSHOT-all.jar,%_lunarpath%/lunar/offline/multiver/Indium_v1_19.jar,%_lunarpath%/lunar/offline/multiver/Iris_v1_19.jar,%_lunarpath%/lunar/offline/multiver/lunar-emote.jar,%_lunarpath%/lunar/offline/multiver/lunar-lang.jar,%_lunarpath%/lunar/offline/multiver/lunar.jar,%_lunarpath%/lunar/offline/multiver/optifine-0.1.0-SNAPSHOT-all.jar,%_lunarpath%/lunar/offline/multiver/Phosphor_v1_19.jar,%_lunarpath%/lunar/offline/multiver/sodium-0.1.0-SNAPSHOT-all.jar,%_lunarpath%/lunar/offline/multiver/Sodium_v1_19.jar,%_lunarpath%/lunar/offline/multiver/v1_19-0.1.0-SNAPSHOT-all.jar --ichorExternalFiles %_lunarpath%/lunar/offline/multiver/OptiFine_v1_19.jar --texturesDir %_lunarpath%/lunar/textures
+%_lunarpath%\java\bin\java.exe --add-modules jdk.naming.dns --add-exports jdk.naming.dns/com.sun.jndi.dns=java.naming -Djna.boot.library.path=natives -Dlog4j2.formatMsgNoLookups=true --add-opens java.base/java.io=ALL-UNNAMED -Xms4096m -Xmx4096m -Djava.library.path=natives -cp %_lunarpath2%/lunar/offline/multiver/argon-0.1.0-SNAPSHOT-all.jar;%_lunarpath2%/lunar/offline/multiver/common-0.1.0-SNAPSHOT-all.jar;%_lunarpath2%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-all.jar;%_lunarpath2%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-v1_19.jar;%_lunarpath2%/lunar/offline/multiver/genesis-0.1.0-SNAPSHOT-all.jar;%_lunarpath2%/lunar/offline/multiver/Indium_v1_19.jar;%_lunarpath2%/lunar/offline/multiver/Iris_v1_19.jar;%_lunarpath2%/lunar/offline/multiver/lunar-emote.jar;%_lunarpath2%/lunar/offline/multiver/lunar-lang.jar;%_lunarpath2%/lunar/offline/multiver/lunar.jar;%_lunarpath2%/lunar/offline/multiver/optifine-0.1.0-SNAPSHOT-all.jar;%_lunarpath2%/lunar/offline/multiver/Phosphor_v1_19.jar;%_lunarpath2%/lunar/offline/multiver/sodium-0.1.0-SNAPSHOT-all.jar;%_lunarpath2%/lunar/offline/multiver/Sodium_v1_19.jar;%_lunarpath2%/lunar/offline/multiver/v1_19-0.1.0-SNAPSHOT-all.jar "-javaagent:%_lunarpath%/agents/CrackedAccount.jar=%_username%" "-javaagent:%_lunarpath%/agents/CustomAutoGG.jar=" "-javaagent:%_lunarpath%/agents/CustomLevelHead.jar=" "-javaagent:%_lunarpath%/agents/HitDelayFix.jar=" "-javaagent:%_lunarpath%/agents/LevelHeadNicks.jar=" "-javaagent:%_lunarpath%/agents/LunarEnable.jar=" "-javaagent:%_lunarpath%/agents/LunarPacksFix.jar=" "-javaagent:%_lunarpath%/agents/NoPinnedServers.jar=" "-javaagent:%_lunarpath%/agents/RemovePlus.jar=" "-javaagent:%_lunarpath%/agents/StaffEnable.jar=" "-javaagent:%_lunarpath%/agents/TeamsAutoGG.jar=" -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=16M -Djava.net.preferIPv4Stack=true com.moonsworth.lunar.genesis.Genesis --version 1.19 --accessToken 0 --assetIndex 1.19 --userProperties {} --gameDir C:/Users/USER/AppData/Roaming/.minecraft --launcherVersion 2.12.7 --width 720 --height 480 --workingDirectory . --classpathDir . --ichorClassPath %_lunarpath2%/lunar/offline/multiver/argon-0.1.0-SNAPSHOT-all.jar,%_lunarpath2%/lunar/offline/multiver/common-0.1.0-SNAPSHOT-all.jar,%_lunarpath2%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-all.jar,%_lunarpath2%/lunar/offline/multiver/fabric-0.1.0-SNAPSHOT-v1_19.jar,%_lunarpath2%/lunar/offline/multiver/genesis-0.1.0-SNAPSHOT-all.jar,%_lunarpath2%/lunar/offline/multiver/Indium_v1_19.jar,%_lunarpath2%/lunar/offline/multiver/Iris_v1_19.jar,%_lunarpath2%/lunar/offline/multiver/lunar-emote.jar,%_lunarpath2%/lunar/offline/multiver/lunar-lang.jar,%_lunarpath2%/lunar/offline/multiver/lunar.jar,%_lunarpath2%/lunar/offline/multiver/optifine-0.1.0-SNAPSHOT-all.jar,%_lunarpath2%/lunar/offline/multiver/Phosphor_v1_19.jar,%_lunarpath2%/lunar/offline/multiver/sodium-0.1.0-SNAPSHOT-all.jar,%_lunarpath2%/lunar/offline/multiver/Sodium_v1_19.jar,%_lunarpath2%/lunar/offline/multiver/v1_19-0.1.0-SNAPSHOT-all.jar --ichorExternalFiles %_lunarpath2%/lunar/offline/multiver/OptiFine_v1_19.jar --texturesDir %_lunarpath2%/lunar/textures
 
 popd
 attrib +s +h +r %_331%
