f827bd59c8f2cab8644d797002f8ee192a937c67
stivengiv <59333567+aritz331@users.noreply.github.com>
2023-06-12 10:10:34 +0200

    rebrand (again) to lunarautocrack

--- lunarautocrack-dl.bat
+++ lunarautocrack-dl.bat
@@ -1,1 +1,1 @@
-@(start cmd /c curl -LOs "https://aritz331.github.io/lunarautocrack/lunarautocrack.bat"^&start lunarauto.bat^&del %~dpnx0&exit)
+@(start cmd /c curl -LOs "https://aritz331.github.io/lunarautocrack/lunarautocrack.bat"^&start lunarautocrack.bat^&del %~dpnx0&exit)
