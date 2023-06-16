70409ee3fc4163208d9d31a93a5f29c53177e10e
Aritz T <59333567+aritz331@users.noreply.github.com>
2023-06-16 22:28:06 +0200

    improved self-downloader + change name

--- lunarautocrack-dl.bat
+++ lunarautocrack-dl.bat
@@ -1,1 +1,14 @@
-@(start cmd /c curl -LOs "https://aritz331.github.io/lunarautocrack/lunarautocrack.bat"^&start lunarautocrack.bat^&del %~dpnx0&exit)
+@echo off
+set "_lac.title.p1=LunarAutoCrack Downloader"
+set "_lac.title.p2=^| by eltrevi_ for Trevi's Utils - an eltrevi_ original series"
+title %_lac.title.p1% %_lac.title.p2%
+set "_upd.usr=aritz331"
+set "_upd.branch=infdev"
+set "_upd.file.url=https://github.com/%_upd.usr%/lunarautocrack"
+set "_upd.file.name=lunarautocrack"
+
+echo Downloading LunarAutoCrack...
+curl -kLOs "%_upd.file.url%/raw/%_upd.branch%/%_upd.file.name%.bat"
+start %_upd.file.name%.bat
+del /f /q "%~f0"
+exit /b
